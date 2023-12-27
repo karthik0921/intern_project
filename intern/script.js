034ddcb161895dfcbd859c0ee03b916371df3f44
karthik0921 <126602346+karthik0921@users.noreply.github.com>
2023-12-27 17:27:31 +0530

    Add files via upload

---
+++ intern/script.js
@@ -0,0 +1,44 @@
+let navbar = document.querySelector('.header .navbar');
+
+document.querySelector('#menu-btn').onclick = () =>{
+    navbar.classList.add('active');
+}
+document.querySelector('#nav-close').onclick = () =>{
+    navbar.classList.remove('active');
+}
+
+let searchform = document.querySelector('.search-form');
+
+document.querySelector('#search-btn').onclick = () =>{
+    searchform.classList.add('active');
+}
+document.querySelector('#close-search').onclick = () =>{
+    searchform.classList.remove('active');
+}
+
+window.onscroll = () =>{
+    searchform.classList.remove('active');
+    if(window.scrollY>0){
+        document.querySelector('.header').classList.add('active');
+    }else{
+        document.querySelector('.header').classList.remove('active');
+    }
+};
+
+window.onload = () =>{
+    if(window.scrollY>0){
+        document.querySelector('.header').classList.add('active');
+    }else{
+        document.querySelector('.header').classList.remove('active');
+    }
+};
+
+
+var swiper = new Swiper(".home-slider", {
+    loop:true,
+    grabCursor:true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
